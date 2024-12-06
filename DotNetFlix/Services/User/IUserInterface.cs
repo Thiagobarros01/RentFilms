989ee40f7314c6be8a4247922ff922ab9c214c2e
unknown <thiagosbarros_@hotmail.com>
2024-12-05 22:03:46 -0300

    adicionado controllers e services

---
+++ DotNetFlix/Services/User/IUserInterface.cs
@@ -0,0 +1,15 @@
+﻿using DotNetFlix.Models;
+
+namespace DotNetFlix.Services.User
+{
+    public interface IUserInterface
+    {
+        Task<ResponseModel<List<UserModel>>> ListarFilmes();
+        Task<ResponseModel<List<UserModel>>> ListarFilmesPorGenero(string genero);
+        Task<ResponseModel<UserModel>> ListarInfoLivro(int id);
+        Task<ResponseModel<UserModel>> AlugarFilme(int id);
+        Task<ResponseModel<List<UserModel>>> AssociarNotaFilmeAlugado();
+        Task<ResponseModel<List<UserModel>>> VisualizarFilmesAlugados();
+
+    }
+}
---
+++ DotNetFlix/Services/User/UserService.cs
@@ -0,0 +1,37 @@
+﻿using DotNetFlix.Models;
+
+namespace DotNetFlix.Services.User
+{
+    public class UserService : IUserInterface
+    {
+        public Task<ResponseModel<UserModel>> AlugarFilme(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<List<UserModel>>> AssociarNotaFilmeAlugado()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<List<UserModel>>> ListarFilmes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<List<UserModel>>> ListarFilmesPorGenero(string genero)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<UserModel>> ListarInfoLivro(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<List<UserModel>>> VisualizarFilmesAlugados()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
